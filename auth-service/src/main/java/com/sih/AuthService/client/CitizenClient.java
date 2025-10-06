5c64f3c522b2172cf31a128ef3e6b7149d03fc9e
Sanskar Patidar <sanskarpatidar00@gmail.com>
2025-10-06 21:58:21 +0530

    testing

--- auth-service/src/main/java/com/sih/AuthService/client/CitizenClient.java
+++ auth-service/src/main/java/com/sih/AuthService/client/CitizenClient.java
@@ -1,11 +1,9 @@
 package com.sih.AuthService.client;
 
-import com.sanskar.sih.citizen.CitizenProfileInterchangeDTO;
 import com.sanskar.sih.citizen.CitizenProfileRequestDTO;
 import com.sanskar.sih.citizen.CitizenProfileResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
--- auth-service/src/main/java/com/sih/AuthService/dto/ValidationResponseDTO.java
+++ auth-service/src/main/java/com/sih/AuthService/dto/ValidationResponseDTO.java
@@ -1,12 +1,9 @@
 package com.sih.AuthService.dto;
 
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
